10db9764d3fa7fee5b9e47aa5a36e44a84385070
Luciano Garriga <lucianomgarriga@gmail.com>
2022-11-10 12:03:17 -0300

    First commit

---
+++ contracts/Player.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Player {
+  
+  string private name;
+  string private position;
+  uint[] private ratings;
+  uint private averageRating;
+  uint private price;
+  
+  constructor() public {
+  }
+
+  function getAverageRating() public {}
+
+  function calculateAverageRating() public {}
+}
---
+++ contracts/Squad.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./Player.sol";
+
+contract Squad {
+
+    string private squadName;
+    Player[] private players;
+
+    constructor() public {}
+}
