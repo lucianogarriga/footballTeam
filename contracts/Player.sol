bae0be4ced761bc58905232d653b989c69dbe9a1
Luciano Garriga <lucianomgarriga@gmail.com>
2022-11-10 17:36:31 -0300

    New functions and events

--- contracts/Player.sol
+++ contracts/Player.sol
@@ -2,17 +2,49 @@
 pragma solidity ^0.8.17;
 
 contract Player {
-  
-  string private name;
-  string private position;
-  uint[] private ratings;
-  uint private averageRating;
-  uint private price;
-  
-  constructor() public {
-  }
+    string private name;
+    string private position;
+    uint256[] private ratings;
+    uint256 private averageRating;
+    uint256 private price;
 
-  function getAverageRating() public {}
+    event RateAdded(uint rate);
 
-  function calculateAverageRating() public {}
+    constructor(
+        string memory _name, 
+        string memory _position, 
+        uint _price) 
+        {
+        name = _name;
+        position = _position;
+        price = _price;
+        }
+    //Se agrega un nuevo Rate al ratings[] y se emite un evento para ver el newRate agregado
+    function addNewRate(uint newRate) public {
+        ratings.push(newRate);
+        calculateAverageRate();
+        emit RateAdded(newRate);
+    }
+    //Una funcion que retorna el valor del averageRating (que se encuentra en una VAR global privada)
+    function getAverageRating() public view returns(uint256) {
+        return averageRating;
+    }
+    //Una funcion get para interactuar de afuera y ver los ratings del array, con una funcion public
+    function getRatings() public view returns(uint[] memory){
+        return ratings;
+    }
+
+    //Otra manera de hacerlo es con una VAR de estado que sea el counter/acumulador (suma de todos los rates)
+    function calculateAverageRate() public {
+        uint amount = 0;
+        for (uint i = 0; i<ratings.length; i++){
+            amount += ratings[i];
+        }
+        averageRating = amount / ratings.length;
+        //este resultado al guardarse en una VAR global, no necesitamos retornar nada
+    }
+    //Funcion getter del price, que se encuentra en una VAR global PRIVATE;
+    function getMarketPrice() public view returns(uint){
+        return price;
+    }
 }
--- contracts/Squad.sol
+++ contracts/Squad.sol
@@ -1,12 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Player.sol";
 
-contract Squad {
+contract Squad is Ownable {
 
-    string private squadName;
+    string private name;
     Player[] private players;
 
-    constructor() public {}
+    event FundsReceived(uint amount);
+    event PlayerAquired();
+
+    receive() external payable {
+        emit FundsReceived(msg.value);
+    }
+
+    fallback() external payable {
+        emit FundsReceived(msg.value);
+    }
+
+    constructor(
+        string memory _name
+    ) {
+        name = _name;
+    }
+    //Funcion p/ agregar un player, toma los parametros definidos x el constructor de Player.sol
+    function addNewPlayer(
+        string memory _name, 
+        string memory _position, 
+        uint _price) public onlyOwner {
+        Player player = new Player(_name, _position, _price);
+        players.push(player);
+    }
+
+    function buyPlayer(address sellerAddress, Player newPlayer) public onlyOwner {
+        require(address(this).balance >= newPlayer.getMarketPrice(), "El equipo no tiene fondos suficientes");
+        (bool sent,) = sellerAddress.call{
+            value: newPlayer.getMarketPrice()
+        }("");
+        require (sent, "Error en la transferencia");
+        players.push(newPlayer);
+        emit PlayerAquired();
+    }
+
+    function getDreamTeam() public {}
+
+    function sellPlayer() public {}
 }
